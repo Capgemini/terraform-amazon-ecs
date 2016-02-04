45e66d76c250128053fb4c9ec652b79e0332980e
Christopher 'Chief' Najewicz <christopher.najewicz@oddnetworks.com>
2016-02-04 11:42:42 -0500

    Update ECS optimized AMI ids

--- variables.tf
+++ variables.tf
@@ -39,12 +39,12 @@ variable "registry_docker_image" {
 /* ECS optimized AMIs per region */
 variable "amis" {
   default = {
-    ap-northeast-1 = "ami-8aa61c8a"
-    ap-southeast-2 = "ami-5ddc9f67"
-    eu-west-1      = "ami-2aaef35d"
-    us-east-1      = "ami-b540eade"
-    us-west-1      = "ami-5721df13"
-    us-west-2      = "ami-cb584dfb"
+    ap-northeast-1 = "ami-aa2d13c4"
+    ap-southeast-2 = "ami-908faaf3"
+    eu-west-1      = "ami-34a21747"
+    us-east-1      = "ami-9886a0f2"
+    us-west-1      = "ami-caf581aa"
+    us-west-2      = "ami-1ead497e"
   }
 }
 
